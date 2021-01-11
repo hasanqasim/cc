4825f9639926f6c76cd24690d2314c1240bca809
Hasan Qasim <hasanqasim92@gmail.com>
2021-01-11 20:05:11 +1100

    Initial Commit

---
+++ TechnicalChallengeCC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalChallengeCC
+//
+//  Created by Hasan Qasim on 11/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
